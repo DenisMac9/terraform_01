3a866b3b71669292b409262b3695a42e3511eb7a
DenisMac9 <d.maclot@treeptik.fr>
2018-08-07 13:57:57 +0200

    delete

--- root.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "com-programmez-terraform" {
-    bucket = "${var.aws_s3_bucket_terraform}"
-    acl    = "private"
-
-    tags {
-        Tool    = "${var.tags-tool}"
-        Contact = "${var.tags-contact}"
-    }
-}
